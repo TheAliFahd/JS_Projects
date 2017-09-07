bc4c256559f0ce6094ed84b264d2962e9c827c1a
awebs <31635844+awebs@users.noreply.github.com>
2017-09-07 11:06:42 -0400

    Create advertisement.js

---
+++ advertisement.js
@@ -0,0 +1,85 @@
+var productName = "Coca Cola";
+var sloganX = 10;
+var sloganY = 85;
+
+var cokeMove = -355;
+
+var sloganText = function(){
+    
+    fill(255, 0, 0);
+    textSize(30);
+    text(productName, 130, 40);
+
+    fill(0, 0, 0);
+    textSize(20);
+    text("Share a ", sloganX, sloganY);
+    textSize(25);
+    fill(255, 0, 0);
+    text("Coke", sloganX + 75, sloganY);
+    fill(0, 0, 0);
+    textSize(20);
+    text(" with a friend!", sloganX + 135, sloganY);
+};
+
+var drawBottle = function(bottleX, bottleY, name){
+    noStroke();
+    
+    // Create a red rectangle for the wide part of the bottle
+    // The rectangle is also curved at the corners
+    fill(255, 0, 0);
+    rect(bottleX, bottleY, 100, 200, 50);
+    
+    // Add white ellipses to "delete"
+    // parts of the bottom of the bottle
+    // This gives it the curved look
+    fill(255, 255, 255);
+    ellipse(bottleX - 75, bottleY + 100, 160, 150);
+    ellipse(bottleX + 175, bottleY + 100, 160, 150);
+    
+    // Add the "share with" name on the bottle
+    var cursive = createFont("Lucida calligraphy", 25);
+    textFont(cursive);
+    text(name, bottleX + 10, bottleY + 50);
+    
+    // Add the top of the bottle
+    fill(255, 0, 0);
+    rect(bottleX, bottleY - 60, 100, 80, 5);
+    
+    // Add white ellipses to curve the top of the bottle
+    fill(255, 255, 255);
+    ellipse(bottleX - 90, bottleY - 30, 225, 150);
+    ellipse(bottleX + 190, bottleY - 30, 225, 150);
+    rect(bottleX + 78, bottleY - 62, 14, 35);
+    rect(bottleX + 8, bottleY - 62, 14, 35);
+
+    // Add bottle cap
+    fill(255, 0, 0);
+    rect(bottleX + 15, bottleY - 95, 70, 35, 8);
+    
+    // Add the angles to the bottle cap with white spaces
+    fill(255, 255, 255);
+    triangle(bottleX + 22, bottleY - 80, bottleX + 10, bottleY - 50             , bottleX + 10, bottleY - 90);
+    triangle(bottleX + 76, bottleY - 80, bottleX + 100, bottleY -              35, bottleX + 100, bottleY - 90);
+
+    // Add slogan beside the bottle
+    var normalFont = createFont("times");
+    textFont (normal);
+    textSize(30);
+    fill(255, 0, 0);
+    text("open happiness", bottleX + 108, bottleY + 50);
+    textSize(8);
+    text("TM", bottleX + 320, bottleY + 35);
+
+};
+
+draw = function() {
+    
+    background(255, 255, 255);
+    sloganText();
+
+    drawBottle(cokeMove, 200, "Mark");
+    if (cokeMove < 40){
+        cokeMove += 2;
+    }
+    
+};
