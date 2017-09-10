a629bafe622712c44ba028e373e65123365163a5
awebs <31635844+awebs@users.noreply.github.com>
2017-09-10 17:18:55 -0400

    Added comments

--- wildAnimal.js
+++ wildAnimal.js
@@ -1,3 +1,4 @@
+// Used to animate
 var y = 110;
 var direction = false;
 
@@ -5,21 +6,23 @@ var direction = false;
 var drawSnowMan = function(){
     background(255, 0, 255);
     
+    // Make background white
     fill(255, 255, 255);
     
-    //Draw the three body parts
+    // Draw the bottom and middle part of the snowman
     
     ellipse(200 + (y - 100), 270, 170, 170);
     ellipse(200, 180, 130, 120);
-    
-    //Draw the features
-    
+
     //Buttons
     fill(0, 0, 0);
     ellipse(200, 160, 10, 10);
     ellipse(200, 190, 10, 10);
     ellipse(200, 220, 10, 10);
 };
+
+// Draws the face of the snowman according to global variables
+// Draws arms as well
 
 var drawFace = function(){
     drawSnowMan();
@@ -35,24 +38,33 @@ var drawFace = function(){
     fill(255, 94, 0);
     triangle(200, y, 200, y + 10, 178 + (y - 100), y + 5);
     
-    //Arms
+    // Arms
+    
+    // left arm
     fill(105, 76, 59);
     rotate(-5);
     rect(240, 180, 92, 10 - (y - 100));
     
+    // right arm
     rotate(10);
     rect(66, 145, 92, 10 - (y - 100));
     
     //Reset the rotation
     rotate(-5);
-
+    
+    // If the current direction is up (true)
+    // take 1 away from the animation variable
+   
     if (direction){
         y --;
     }
+    
+    // else we are animating down
     else{
         y ++;
     }
-
+    
+    // change animation direction variable based on a y stopping point
     if (y  ===  115){
         direction = true;
     }
@@ -61,8 +73,7 @@ var drawFace = function(){
     }
 };
 
-
-
+// Continuously call the drawFace function
 draw = function() {
     drawFace();
 };
